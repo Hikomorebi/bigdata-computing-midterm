39358c34f053724fcc958bc9ec26b09183147481
1911417-He Kunbin <1207769645@qq.com>
2022-05-07 21:23:54 +0800

    完成

--- bs2.py
+++ bs2.py
@@ -6,19 +6,13 @@ __author__ = 'komorebi'
 import pickle
 import os
 
-
 tmp_dict = {}
 iter_dict = {}
 out_count = {}
-no_out_node = set() #出度为0的节点集合，即deadend
-no_in_node = set()
-
-no_in_node_list = []
 node_count = 0
 teleport_par = 0.85
 
-for _ in range(10):
-    no_in_node_list.append(set())
+
 
 if os.path.exists('./tmp/node_set'):
     with open('./tmp/node_set', 'rb') as f1:
@@ -26,15 +20,9 @@ if os.path.exists('./tmp/node_set'):
 else:
     print('未找到/tmp/node_set文件，请先运行bs.py')
 
-
-with open('./tmp/no_out_node','rb') as f:
-    no_out_node = pickle.load(f)
-with open('./tmp/no_in_node','rb') as f:
-    no_in_node = pickle.load(f)
 node_count = len(node_set)
 
-for _ in no_in_node:
-    no_in_node_list[_ // 1000].add(_)
+
 '''
 接下来获取每一个节点的出度
 '''
@@ -46,36 +34,40 @@ for _ in range(10):
                 out_count[key] = len(tmp_dict[key])
             tmp_dict.clear()
 
-# for _ in node_set:    #输出每个节点对应的出度
-#     print(str(_)+':'+str(out_count[_]))
-#print(len(no_out_node))
-
-
-'''
-计算迭代矩阵，并将该矩阵分块存储
-'''
-for _ in range(10):
-    if os.path.exists('./tmp/in_table' + str(_)):
-        with open('./tmp/in_table' + str(_), 'rb') as f:
-            tmp_dict = pickle.load(f)
-        for to_node in tmp_dict.keys():
-            iter_dict[to_node] = {}
-            for from_node in node_set:
-                if from_node in tmp_dict[to_node]:
-                    iter_dict[to_node][from_node] = teleport_par * 1 / out_count[from_node] + (1 - teleport_par) * 1 / node_count
-                elif from_node in no_out_node:
-                    iter_dict[to_node][from_node] = 1 / node_count
-                else:
-                    iter_dict[to_node][from_node] = (1 - teleport_par) * 1 / node_count
-        for to_node in no_in_node_list[_]:  #处理入度为0的节点
-            iter_dict[to_node] = {}
-            for from_node in node_set:
-                if from_node in no_out_node:
-                    iter_dict[to_node][from_node] = 1 / node_count
-                else:
-                    iter_dict[to_node][from_node] = (1 - teleport_par) * 1 / node_count
-        with open('./tmp/iter_table'+str(_),'wb+') as f:
-            pickle.dump(iter_dict,f)
-        iter_dict.clear()
-
+stop_loss = 0.000001
+loss = 999
+old_rank = {}
+new_rank = {}
+for _ in node_set:
+    old_rank[_] = 1 / node_count
+    new_rank[_] = 0
+count = 20
+while count>0:
+    count -= 1
+    for _ in range(10):
+        if os.path.exists('./tmp/in_table' + str(_)):
+            with open('./tmp/in_table' + str(_), 'rb') as f:
+                tmp_dict = pickle.load(f)
+            for to_node in tmp_dict.keys():
+                sum = 0
+                for from_node in tmp_dict[to_node]:
+                    sum += teleport_par * (old_rank[from_node] / out_count[from_node])
+                new_rank[to_node] = sum
+            tmp_dict.clear()
+    acc = 0
+    for _ in node_set:
+        acc += new_rank[_]
+    temp = (1 - acc) / node_count
+    for _ in node_set:
+        new_rank[_] += temp
+    loss = 0
+    for _ in node_set:
+        loss += abs(old_rank[_] - new_rank[_])
+        old_rank[_] = new_rank[_]
+        new_rank[_] = 0
+sort_list = sorted(old_rank.items(), key=lambda x: x[1], reverse=True)
+with open('./result.txt','w') as f:
+    for _ in range(100):
+        str1 = str(sort_list[_][0]) + ' ' + str(sort_list[_][1]) + '\n'
+        f.write(str1)
 
